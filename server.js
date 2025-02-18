20262e966f88b87198dfd39781cdddb4da0e1c53
Jano448 <alex-ortega@live.com.mx>
2025-02-18 15:30:28 -0600

    Server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>HELLO WORLD</h1>");
 });
 
 app.listen(3000);
