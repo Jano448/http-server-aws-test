a178642652ede4cc25389f98d53d20b27cc7de7f
Jano448 <alex-ortega@live.com.mx>
2025-02-18 15:58:03 -0600

    Server aws2

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>HELLO WORLD</h1>");
-  res.send("<p>Ejecutando desde linux en AWS</p>");
+  res.send(`<h1>HELLO WORLD</h1><p>Esto es una prueba de AWS</p>`);
 });
 
 app.listen(3000);
