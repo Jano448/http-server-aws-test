0f2c93b852ca703471f29d92ad0f7ba8b7af89f8
Jano448 <alex-ortega@live.com.mx>
2025-02-18 15:14:08 -0600

    Second

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World\n");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(3000);
+app.listen(3000);
 console.log("Server running at port 3000");
